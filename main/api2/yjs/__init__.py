3e7f7b45907f89eb81964e1c9b3227c2ffaeb283
qkyzs <945999685@qq.com>
2019-03-07 11:53:44 +0800

    增加教学计划查询

--- main/api2/yjs/__init__.py
+++ main/api2/yjs/__init__.py
@@ -4,7 +4,19 @@
 @author: qkyzs
 @license: (C) Copyright 2017-2018, Node Supply Chain Manager Corporation Limited.
 @contact: nepu1960@yeah.net
-@file: __init__.py.py
-@time: 2019/3/5 0005 16:14
+@file: routes.py
+@time: 2019/3/6 0006 15:07
 @desc:
 """
+from . import api
+import requests
+
+@api.route('/' )
+def login():
+    appid='wx2d6c803a134581e6'
+    secret='d20ddfe3841261ea72ea15df1254fd60'
+    JSCODE=''
+    url='https://api.weixin.qq.com/sns/jscode2session?appid='+appid+'&secret='+secret+'&js_code=JSCODE&grant_type=authorization_code'
+    code2Session=requests.get(url).text
+
+    return 'yes'
